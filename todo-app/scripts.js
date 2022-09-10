a32da88e474446761f5aec17e91868e9f6569b9c
Siddhesh Ranade <ranade.siddhesh@gmail.com>
2022-09-10 14:14:30 -0400

    Add functionality for todo

--- todo-app/scripts.js
+++ todo-app/scripts.js
@@ -1,1 +1,50 @@
-console.log('Hello!');
+const formElement = document.querySelector('form');
+const inputTextElement = document.querySelector('#input-text');
+const listParentElement = document.querySelector('#todo-list');
+
+formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodoListElement();
+});
+
+let storedTodoItems = JSON.parse(localStorage.getItem('todo'));
+if (storedTodoItems && storedTodoItems.length) {
+    storedTodoItems.forEach(item => addTodoListElement(item));
+}
+
+function addTodoListElement(todoItem) {
+    const listElement = document.createElement('li');
+    listElement.innerHTML = todoItem && todoItem['name'] ? todoItem['name'] : inputTextElement.value;
+    if (todoItem && todoItem['completed'] === 'yes') {
+        listElement.classList.add('cancelled-task');
+    }
+    listParentElement.appendChild(listElement);
+    inputTextElement.value = '';
+
+    updateLocalStorage();
+
+    listElement.addEventListener('click', () => {
+        listElement.classList.toggle('cancelled-task');
+        updateLocalStorage();
+    });
+
+    listElement.oncontextmenu = function() {
+        listParentElement.removeChild(listElement);
+        updateLocalStorage();
+        return false; /* So the right click menu doesn't appear */
+    }
+}
+
+function updateLocalStorage() {
+    const listElements = document.querySelectorAll('li');
+    let listItems = [];
+    listElements.forEach(el => {
+        console.log(el.classList);
+        listItems.push({
+            'name' : el.innerHTML,
+            'completed' : el.classList.contains('cancelled-task') ? 'yes' : 'no'
+        });
+    });
+
+    localStorage.setItem('todo', JSON.stringify(listItems));
+}
